51e011a3240f5e58cc263916d3ce75e6c8e912ea
xiaofan0208 <xiaofan0208@126.com>
2018-10-23 13:46:47 +0000

    管理后台 - 订单列表

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -18,4 +18,7 @@ Route::group([
     $router->post('products', 'ProductsController@store');
     $router->get('products/{id}/edit','ProductsController@edit'); // 编辑 后台商品
     $router->put('products/{id}', 'ProductsController@update');
+
+    $router->get('orders', 'OrdersController@index')->name('admin.orders.index');
+    
 });
